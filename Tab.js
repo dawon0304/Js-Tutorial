a6bb57acedfd22df783442e571a11581511fbb11
dawon0304 <dawon0304@naver.com>
2022-04-05 16:29:24 +0900

    for반복문 이용한 tab기능

--- Tab.js
+++ Tab.js
@@ -4,27 +4,31 @@
 // -모든 tab-content에 show클래스 제거
 // - 0 번째 content에 show클래스 추가
 
-  var 버튼 = $('.tab-button');
-  var 내용 = $('.tab-content')
-  버튼.eq(0).on('click', function(){
-  버튼.removeClass('orange');
-  버튼.eq(0).addClass('orange');
-  내용.removeClass('show');
-  내용.eq(0).addClass('show');
+for (let i = 0; i<3; i++){
+    var 버튼 = $('.tab-button');
+    var 내용 = $('.tab-content')
+    버튼.eq(i).on('click', function(){
+    버튼.removeClass('orange');
+    버튼.eq(i).addClass('orange');
+    내용.removeClass('show');
+    내용.eq(i).addClass('show');
 })
+}
 
-$('.tab-button').eq(1).on('click', function(){
-  $('.tab-button').removeClass('orange');
-  $('.tab-button').eq(1).addClass('orange');
-  $('.tab-content').removeClass('show');
-  $('.tab-content').eq(1).addClass('show');
-})
 
-$('.tab-button').eq(2).on('click', function(){
-  $('.tab-button').removeClass('orange');
-  $('.tab-button').eq(2).addClass('orange');
-  $('.tab-content').removeClass('show');
-  $('.tab-content').eq(2).addClass('show');
-})
 
-// 선택자 마다 코드를 다 짤 수 없으니 함수를 만들어야한다.
+// $('.tab-button').eq(1).on('click', function(){
+//   $('.tab-button').removeClass('orange');
+//   $('.tab-button').eq(1).addClass('orange');
+//   $('.tab-content').removeClass('show');
+//   $('.tab-content').eq(1).addClass('show');
+// })
+
+// $('.tab-button').eq(2).on('click', function(){
+//   $('.tab-button').removeClass('orange');
+//   $('.tab-button').eq(2).addClass('orange');
+//   $('.tab-content').removeClass('show');
+//   $('.tab-content').eq(2).addClass('show');
+// })
+
+
